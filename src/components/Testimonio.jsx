32b82641ce6a3a4711ad5e57555f668f84821fdc
Ivan Montenegro <nelson.moncar@outlook.com>
2022-09-29 16:14:29 -0500

    creación del primer modulo de testimonios y guardado de las imagenes

---
+++ src/components/Testimonio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Testimonio(){
+    return (
+        <div className='contenedor-testimonio'>
+            <img 
+                className='imagen-testimonio' 
+                src={require('../images/testimonio-emma.png')} 
+                alt="imagen-testimonio" />
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+    );
+}
