8281e61894da3807938dce809776bfa3147fd255
Ivan Montenegro <nelson.moncar@outlook.com>
2022-09-30 15:44:42 -0500

    creación de los 3 componentes

--- src/Testimonio/index.jsx
+++ src/Testimonio/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import './testimonio.css';
 
-function Testimonio(){
+function Testimonio(props){
     return (
         <div className='contenedor-testimonio'>
             <img 
                 className='imagen-testimonio' 
-                src={require('../images/testimonio-emma.png')} 
+                src={require(`../images/testimonio-${props.image}.png`)} 
                 alt='imagen-testimonio' />
             <div className='contenedor-text-testimonio'>
-                <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-                <p className='Cargo-testimonio'>ingeniera de Software en Spotify</p>
-                <p className='texto-del-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+                <p className='nombre-testimonio'>{props.name} en {props.country}</p>
+                <p className='Cargo-testimonio'>{props.job} en {props.company}</p>
+                <p className='texto-del-testimonio'>"{props.testimony}"</p>
                 
             </div>
         </div>
