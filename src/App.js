6b7493d3eda57a4bf8cf5db08a3e758cc0803698
Ivan Montenegro <nelson.moncar@outlook.com>
2022-09-30 15:13:05 -0500

    agregar archivos css y separación de componentes por carpetas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {Testimonio} from './Testimonio';
+import {Titulo} from './Titulo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='contenedor-principal'>
+        <Titulo/>
+        <Testimonio/>
+      </div>
     </div>
   );
 }
---
+++ src/Titulo/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './titulo.css'
+
+function Titulo (){
+    return(
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+    )
+    
+}
+
+export {Titulo};
